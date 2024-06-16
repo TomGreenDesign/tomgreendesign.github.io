0578af8d7d917590b6c8ee611676cd8bafcc8f36
TomGreenDesign <design@tomgreen.uk>
2024-06-16 17:03:38 +0100

    Resync

--- header.js
+++ header.js
@@ -48,23 +48,23 @@ class navBar extends HTMLElement {
 								<div class="col-1">
 									<svg xmlns="http://www.w3.org/2000/svg" class="linkIcon vertical-align:middle"
 										viewBox="0 0 24 24" height="1em">
-										<path fill="#ff3864"
+										<path fill="#EA5637"
 											d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" />
 									</svg>
 								</div>
 								<div class="col">
-									<a href="mailto:design@tomgreen.uk">design@tomgreen.uk </a>
+									<a style="color:inherit" href="mailto:design@tomgreen.uk">design@tomgreen.uk </a>
 								</div>
 
 								<div class="row">
 									<div class="col-1">
 										<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 24 24">
-											<path fill="#ff3864"
+											<path fill="#EA5637"
 												d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
 										</svg>
 									</div>
 									<div class="col">
-										<a href="https://www.linkedin.com/in/tom-jgreen/">
+										<a style="color:inherit" href="https://www.linkedin.com/in/tom-jgreen/">
 											linkedin.com/in/tom-jgreen
 										</a>
 									</div>
@@ -74,7 +74,7 @@ class navBar extends HTMLElement {
 							<div class="row">
 								<div class="col-1">
 									<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 24 24">
-										<path fill="#ff3864"
+										<path fill="#EA5637"
 											d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
 									</svg>
 								</div>
